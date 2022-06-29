bcaadbdc7f6ae3b32610f2b22cceb862d272284d
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-06-30 01:16:15 +0200

    check-config-server-started.sh added.

--- config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .antMatchers("/actuator/**")
                 .antMatchers("/encrypt/**")
                 .antMatchers("/decrypt/**");
         super.configure(web);
