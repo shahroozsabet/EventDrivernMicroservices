eda7b796bd77401868298ed3e28d61566cc7a752
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-03 22:52:15 +0200

    TwitterElasticRepositoryIndexClient.java added.

--- elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticIndexClient.java
+++ elastic/elastic-index-client/src/main/java/com/microservices/demo/elastic/index/client/service/impl/TwitterElasticIndexClient.java
@@ -6,6 +6,7 @@ import com.microservices.demo.elastic.index.client.util.ElasticIndexUtil;
 import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "elastic-config.is-repository", havingValue = "false")
 public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterIndexModel> {
 
     private final ElasticConfigData elasticConfigData;
