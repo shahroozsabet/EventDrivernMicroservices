b757a99727dbe5ee342dc0838d795cc4a862a117
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-08 16:25:41 +0200

    TwitterElasticQueryWebClient added.

--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/api/QueryController.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/api/QueryController.java
@@ -2,6 +2,8 @@ package com.microservices.demo.elastic.query.web.client.api;
 
 import com.microservices.demo.elastic.query.web.client.model.ElasticQueryWebClientRequestModel;
 import com.microservices.demo.elastic.query.web.client.model.ElasticQueryWebClientResponseModel;
+import com.microservices.demo.elastic.query.web.client.service.ElasticQueryWebClient;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class QueryController {
+
+    private final ElasticQueryWebClient elasticQueryWebClient;
 
     @GetMapping("")
     public String index() {
@@ -37,11 +41,7 @@ public class QueryController {
     public String queryByText(@Valid ElasticQueryWebClientRequestModel requestModel,
                               Model model) {
         log.info("Querying with text {}", requestModel.getText());
-        List<ElasticQueryWebClientResponseModel> responseModels = new ArrayList<>();
-        responseModels.add(ElasticQueryWebClientResponseModel.builder()
-                .id("1")
-                .text(requestModel.getText())
-                .build());
+        List<ElasticQueryWebClientResponseModel> responseModels = elasticQueryWebClient.getDataByText(requestModel);
         model.addAttribute("elasticQueryWebClientResponseModels", responseModels);
         model.addAttribute("searchText", requestModel.getText());
         model.addAttribute("elasticQueryWebClientRequestModel",
