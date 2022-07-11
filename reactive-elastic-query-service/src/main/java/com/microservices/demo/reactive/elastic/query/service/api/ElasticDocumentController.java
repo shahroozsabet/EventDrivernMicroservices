5a38edbaf62406420678a84e47560efb044a10da
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-11 11:36:25 +0200

    reactive-elastic-query-service added.

---
+++ reactive-elastic-query-service/src/main/java/com/microservices/demo/reactive/elastic/query/service/api/ElasticDocumentController.java
@@ -0,0 +1,35 @@
+package com.microservices.demo.reactive.elastic.query.service.api;
+
+import com.microservices.demo.elastic.query.service.common.model.ElasticQueryServiceRequestModel;
+import com.microservices.demo.elastic.query.service.common.model.ElasticQueryServiceResponseModel;
+import com.microservices.demo.reactive.elastic.query.service.business.ElasticQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/documents")
+@Slf4j
+@RequiredArgsConstructor
+public class ElasticDocumentController {
+
+    private final ElasticQueryService elasticQueryService;
+
+    @PostMapping(value = "/get-doc-by-text",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<ElasticQueryServiceResponseModel> getDocumentByText(
+            @RequestBody @Valid ElasticQueryServiceRequestModel requestModel) {
+        Flux<ElasticQueryServiceResponseModel> response = elasticQueryService.getDocumentByText(requestModel.getText());
+        response = response.log();
+        log.info("Returning from query reactive service for text {}!", requestModel.getText());
+        return response;
+    }
+}
