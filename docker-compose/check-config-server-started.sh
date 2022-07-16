28d5770f81fdf8ac9e04edbfc4470894157b5c23
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-16 16:01:17 +0200

    services.yml with SSO.

--- docker-compose/check-config-server-started.sh
+++ docker-compose/check-config-server-started.sh
@@ -10,9 +10,9 @@ curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888
 echo "result status code:" "$curlResult"
 
 while [[ ! $curlResult == "200" ]]; do
-  echo >&2 "Config server is not up yet!"
+  echo >&2 "Config server is not up yet."
   sleep 2
   curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
 done
 
-/cnb/process/web
+check-keycloak-server-started.sh
--- docker-compose/check-kafka-topics-created.sh
+++ docker-compose/check-kafka-topics-created.sh
@@ -10,7 +10,7 @@ kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 echo "kafkacat result:" $kafkacatResult
 
 while [[ ! $kafkacatResult == *"twitter-topic"* ]]; do
-  echo >&2 "Kafka topic has not been created yet!"
+  echo >&2 "Kafka topic has not been created yet."
   sleep 2
   kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 done
